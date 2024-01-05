b01a2e7a460a00eb1fd41ae476e00867977ad082
ShahandFahad <shahandfahadmad@gmail.com>
2024-01-06 02:09:14 +0500

    Updated config tailwind

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,17 +5,18 @@ import Navbar2 from "../../components/Navbar/Navbar2";
 import SuggestCategories from "../../components/SuggestCategories/SuggestCategories";
 import RecentProducts from "../../components/RecentProducts/RecentProducts";
 import Footer from "../../components/Footer/Footer";
+// TODO: Change the Navbar. Add Search and Other Links
 function Home() {
   return (
     <>
       <Navbar />
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <Navbar2/>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Navbar2 />
       <SuggestCategories />
       <RecentProducts title="Recent Products" items={10} />
       <Footer />
