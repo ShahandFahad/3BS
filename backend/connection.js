5d66d3fbc8592874a12cf2f5ad2bd41eebfcbecd
ShahandFahad <shahandfahadmad@gmail.com>
2024-01-19 18:31:30 +0500

    Setting up Local DB Connection

--- backend/connection.js
+++ backend/connection.js
@@ -1,12 +1,29 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+// Set path to environment variable file
+dotenv.config({ path: "./.env" });
+
+// Create Database Connection
+// const connectToDatabase = async () => {
+//   try {
+//     const connection = await mongoose.connect(process.env.DB_URI);
+//     console.log("connected Successfully!!");
+//   } catch (err) {
+//     console.log("connection failed");
+//   }
+// };
+
+// DB connection
+const connString = process.env.DB_URI;
 
 // Create Database Connection
 const connectToDatabase = async () => {
   try {
-    const connection = await mongoose.connect(process.env.DB_URI);
-    console.log("connected Successfully!!");
+    await mongoose.connect(connString, { family: 4 });
+    console.log("DB Connection Successfull !!!");
   } catch (err) {
-    console.log("connection failed");
+    console.log("DB CONNECTION FAILED");
   }
 };
 
