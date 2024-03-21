d0a7d044026188fc78badc7e4c54ae18321de489
ShahandFahad <dev.shahfahad@gmail.com>
2024-03-21 13:59:19 +0500

    Updated row style

--- src/components/AllCurrentUserProducts/SingleCurrentUserProduct/SingleCurrentUserProduct.js
+++ src/components/AllCurrentUserProducts/SingleCurrentUserProduct/SingleCurrentUserProduct.js
@@ -5,6 +5,7 @@ import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { publicRequest, userRequest } from "../../../requestMethods";
 import CloseIcon from "@mui/icons-material/Close";
 import { Link } from "react-router-dom";
+import zIndex from "@mui/material/styles/zIndex";
 
 function SingleCurrentUserProduct({ product, mode }) {
   const [modal, setModal] = useState(false);
@@ -66,97 +67,162 @@ function SingleCurrentUserProduct({ product, mode }) {
               status,
             }
           )) && setModal(false);
+      /**
+       * When The status for product exchange is changed. Refreset the page
+       * This not an efficient way to do it. Just for Temprory purpose.
+       * In, Future Implement a state refresh. The react way. of doing it.
+       */
+      window.location.reload();
     } catch (err) {
       console.log(err.response.data);
     }
   };
 
   return (
-    <div className="single__current__user__product">
-      <img src={product.photos[0]} />
-      <p className="title">{product.title}</p>
-      <div className="flex gap-2 bg-gray-700 p-2 text-sm text-white rounded">
-        <span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="w-6 h-6"
-          >
-            <path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
-            <path
-              fill-rule="evenodd"
-              d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 0 1 0-1.113ZM17.25 12a5.25 5.25 0 1 1-10.5 0 5.25 5.25 0 0 1 10.5 0Z"
-              clip-rule="evenodd"
-            />
-          </svg>
-        </span>{" "}
-        <span>{productViews?.length}</span>
-      </div>
-      {product.condition && <p className="title">{product.condition}</p>}
-      <p className="price">
-        {currencyFormatter.format(product.price, { code: "" })}
-        <span>(Pkr)</span>
-      </p>
-      <Link
-        to={
-          product.status === "exchange" &&
-          `/exchangeproductdetails/${product._id}`
-        }
-        className="status"
-        style={
-          product.status === "Sold" ||
-          product.status === "exchanged" ||
-          product.status === "sold"
-            ? { backgroundColor: "#111827" }
-            : { backgroundColor: "#22c55e" }
-        }
+    <tr class="bg-white border-b hover:bg-gray-50">
+      <td class="w-4 p-4">
+        <div class="flex items-center">
+          <img
+            class="w-10 h-10 rounded-full"
+            src={product.photos[0]}
+            alt="EX-Img"
+          />
+        </div>
+      </td>
+      <th
+        scope="row"
+        class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap"
       >
-        {product.status === "exchange" ? (
-          "EXCHANGE NOW"
-        ) : (
-          <span className="font-bold">{product.status.toUpperCase()}</span>
-        )}
-      </Link>
+        <div class="ps-3">
+          <div class="text-base font-semibold">{product.title}</div>
+          <div class="font-normal text-gray-500">{productDate}</div>
+        </div>
+      </th>
+      <td class="px-6 py-4">👁 {productViews?.length}</td>
+      <td class="px-6 py-4">
+        {currencyFormatter.format(product.price, { code: "" })}
+      </td>
+      <td class="px-6 py-4">
+        {product.condition && <span>{product.condition}</span>}
+      </td>
 
-      <p className="createdAt">{productDate}</p>
-      {/* more icon */}
-      <MoreVertIcon onClick={() => setModal(!modal)} />
-      {modal &&
-        (mode === "sell" ? (
-          <div className="modal">
-            <>
-              <p onClick={() => removeProduct("sell")}>Delete</p>
-              <Link to={`/editproduct/${product._id}`}>
-                <p>Edit</p>
-              </Link>
-            </>
-            <CloseIcon onClick={() => setModal(false)} />
-          </div>
-        ) : (
-          <div className="modal">
-            <>
-              <p onClick={() => removeProduct("exchange")}>Delete</p>
-              <Link to={`/editexchangeproduct/${product._id}`}>
-                <p>Edit</p>
-              </Link>
+      <td class="px-6 py-4">
+        <div class="flex items-center">
+          {/* <div class="h-2.5 w-2.5 rounded-full bg-green-500 me-2"></div>{" "}
+                    Online */}
+          <Link
+            to={
+              product.status === "exchange" &&
+              `/exchangeproductdetails/${product._id}`
+            }
+          >
+            <span class="flex items-center text-gray-500">
+              <div
+                class={`h-2.5 w-2.5 rounded-full me-2
+              ${
+                product.status === "Sold" ||
+                product.status === "exchanged" ||
+                product.status === "sold"
+                  ? "bg-red-500"
+                  : "bg-green-500"
+              }`}
+              ></div>
+
+              {/* style=
+               */}
               {product.status === "exchange" ? (
-                <p onClick={() => handleExchangeStatus("exchanged")}>
-                  Mark as Exchanged
-                </p>
+                <>Exchange</>
               ) : (
-                <p onClick={() => handleExchangeStatus("exchange")}>
-                  Mark as Exchange
-                </p>
+                <>{product.status.toUpperCase()}</>
               )}
-              <Link to={`/exchangeproductdetails/${product._id}`}>
-                <p>Exchange NOW</p>
-              </Link>
-            </>
-            <CloseIcon onClick={() => setModal(false)} />
-          </div>
-        ))}
-    </div>
+            </span>
+          </Link>
+        </div>
+      </td>
+
+      {/* Model for More Details */}
+      <div class="relative">
+        <div class="absolute top-0 right-10 mt-[-10px] w-full z-10 overflow-y-auto max-h-screen">
+          {modal && (
+            <div class="bg-white border border-gray-200 rounded-lg py-2">
+              {mode === "sell" ? (
+                <>
+                  <p
+                    onClick={() => removeProduct("sell")}
+                    class="px-4 py-2 cursor-pointer hover:bg-gray-100"
+                  >
+                    Delete
+                  </p>
+                  <Link
+                    to={`/editproduct/${product._id}`}
+                    class="px-4 py-2 block cursor-pointer hover:bg-gray-100"
+                  >
+                    Edit
+                  </Link>
+                  <p
+                    class="px-4 py-2 cursor-pointer hover:bg-gray-100"
+                    onClick={() => setModal(false)}
+                  >
+                    Close
+                  </p>
+                </>
+              ) : (
+                <>
+                  <p
+                    onClick={() => removeProduct("exchange")}
+                    class="px-4 py-2 cursor-pointer hover:bg-gray-100"
+                  >
+                    Delete
+                  </p>
+                  <Link
+                    to={`/editexchangeproduct/${product._id}`}
+                    class="px-4 py-2 block cursor-pointer hover:bg-gray-100"
+                  >
+                    Edit
+                  </Link>
+                  {product.status === "exchange" ? (
+                    <p
+                      onClick={() => handleExchangeStatus("exchanged")}
+                      class="px-4 py-2 cursor-pointer hover:bg-gray-100"
+                    >
+                      Mark as Exchanged
+                    </p>
+                  ) : (
+                    <p
+                      onClick={() => handleExchangeStatus("exchange")}
+                      class="px-4 py-2 cursor-pointer hover:bg-gray-100"
+                    >
+                      Mark as Exchange
+                    </p>
+                  )}
+                  <Link
+                    to={`/exchangeproductdetails/${product._id}`}
+                    class="px-4 py-2 block cursor-pointer hover:bg-gray-100"
+                  >
+                    Exchange NOW
+                  </Link>
+                  <p
+                    class="px-4 py-2 cursor-pointer hover:bg-gray-100"
+                    onClick={() => setModal(false)}
+                  >
+                    Close
+                  </p>
+                </>
+              )}
+            </div>
+          )}
+        </div>
+        <td class="relative px-6 py-4">
+          <a
+            href="##"
+            class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+          >
+            {/* Show More Icon */}
+            <MoreVertIcon onClick={() => setModal(!modal)} />
+          </a>
+        </td>
+      </div>
+    </tr>
   );
 }
 
